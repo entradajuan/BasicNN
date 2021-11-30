f2861fcbc78df6beef994ca06bc9eeb4aa18fec0
entradajuan <entradajuan@yahoo.es>
2021-11-30 18:27:37 +0000

    ada

--- ADAGD.py
+++ ADAGD.py
@@ -1,3 +1,36 @@
 from numpy.random import seed
 
 
+class AdaSGD(Object):
+  
+  def __init__():
+
+
+  def fit():
+
+
+
+  def partial_fit():
+
+
+  
+  def _shuffle():
+
+
+
+  def _initialize_weights():
+
+
+
+  def _update_weights():
+
+
+
+  def net_input():
+
+
+  def activation():
+
+  
+  def predict(self, X):
+
