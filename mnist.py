481232c4be8cc3f7a845515244d223ac58482324
entradajuan <entradajuan@yahoo.es>
2021-12-01 19:04:17 +0000

    basic NN

--- mnist.py
+++ mnist.py
@@ -2,6 +2,9 @@ import tensorflow as tf
 import numpy as np
 from tensorflow import keras
 
+
+NB_CLASSES = 10
+
 mnist = keras.datasets.mnist
 (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
 
@@ -23,10 +26,41 @@ print(X_test.shape)
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
 
-X_train = X_train/ 255
-X_test = X_test/ 255
+X_train = X_train/ 255.0
+X_test = X_test/ 255.0
 
-X_train[0]
+print(Y_train[0])
+
+Y_train = keras.utils.to_categorical(Y_train, NB_CLASSES)
+Y_test =  keras.utils.to_categorical(Y_test, NB_CLASSES)
+
+print(Y_train[0])
+
+N_HIDDEN = 25
+DROPOUT = 0.15
+
+#build the model
+model = tf.keras.models.Sequential()
+
+model.add(keras.layers.Dense(100,input_shape=(RESHAPED,),
+   		name='input_layer', activation='relu'))
+model.add(keras.layers.Dense(NB_CLASSES,
+   		name='output_layer', activation='softmax'))
+
+model.summary()
+
+model.compile(optimizer='Adam', 
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+BATCH_SIZE = 50
+EPOCHS = 15
+VERBOSE = True 
+VALIDATION_SPLIT = 0.2
+
+model.fit(X_train, Y_train, batch_size=BATCH_SIZE, epochs=EPOCHS,
+		verbose=VERBOSE, validation_split=VALIDATION_SPLIT)
+
 
 
 
