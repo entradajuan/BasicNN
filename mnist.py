5f70f7f9a797682b7c6c5991511c0202be1a1bf0
entradajuan <entradajuan@yahoo.es>
2021-12-01 18:25:37 +0000

    basic NN

--- mnist.py
+++ mnist.py
@@ -2,7 +2,11 @@ import tensorflow as tf
 import numpy as np
 from tensorflow import keras
 
+mnist = keras.datasets.mnist
+(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
 
+print(type(X_train))
+print(X_train.shape)
 
 
 
