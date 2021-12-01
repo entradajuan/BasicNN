30a131a77cd15d1ec6298ea61648fbff709c0e7d
entradajuan <entradajuan@yahoo.es>
2021-12-01 18:37:42 +0000

    basic NN

--- mnist.py
+++ mnist.py
@@ -8,5 +8,25 @@ mnist = keras.datasets.mnist
 print(type(X_train))
 print(X_train.shape)
 
+image = X_train[0]
+print(image.shape)
+RESHAPED = image.shape[0] * image.shape[1]
+
+print(RESHAPED)
+
+print(X_train.shape)
+X_train = X_train.reshape(X_train.shape[0], RESHAPED)
+print(X_train.shape)
+X_test = X_test.reshape(X_test.shape[0], RESHAPED)
+print(X_test.shape)
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+
+X_train = X_train/ 255
+X_test = X_test/ 255
+
+X_train[0]
+
 
 
